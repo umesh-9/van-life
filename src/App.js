d5488373c0a565a8c9335ec367ea0ea240405d61
umesh-9 <umeshnaidu00@gmail.com>
2023-05-30 19:28:10 +0530

    delete unused code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { ReactDOM } from "react";
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 function App() {
