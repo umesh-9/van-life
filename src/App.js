f14fde72ab2898d14695f74a7577c1df7c6a4fc0
umesh-9 <umeshnaidu00@gmail.com>
2023-05-30 19:04:55 +0530

    adding images and created home&about components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { ReactDOM } from "react";
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VANLIFE
+        </Link>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
       </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
