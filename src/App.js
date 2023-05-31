c089b79bf0c14bf25bf3448362c6117fd36b2dd1
umesh-9 <umeshnaidu00@gmail.com>
2023-05-31 11:56:54 +0530

    adding vans pages to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
+import Vans from "./Components/Vans";
+import VanDetail from "./VanDetail";
+
+import "./server";
 function App() {
   return (
     <BrowserRouter>
@@ -10,13 +14,15 @@ function App() {
           #VANLIFE
         </Link>
         <nav>
-          <Link to="/">Home</Link>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
