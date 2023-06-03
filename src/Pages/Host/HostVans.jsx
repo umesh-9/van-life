749d26ec108bbd8bef6328119800dca0fa065026
umesh-9 <umeshnaidu00@gmail.com>
2023-06-03 12:21:41 +0530

    alt prop changed

--- src/Pages/Host/HostVans.jsx
+++ src/Pages/Host/HostVans.jsx
@@ -17,7 +17,7 @@ const Vans = () => {
       className="host-van-link-wrapper"
     >
       <div className="host-van-single" key={van.id}>
-        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+        <img src={van.imageUrl} alt="" />
         <div className="host-van-info">
           <h3>{van.name}</h3>
           <p>${van.price}/day</p>
